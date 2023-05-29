6a179a535172733fe9d50ecb8ad2cb218de59c9f
daiant <cmg2512@gmail.com>
2023-05-29 20:15:19 +0200

    feat(products): usage of featured and info

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "@/components/hero/hero";
 import Parallax from "@/components/parallax/parallax";
 import Featured from "./components/featured/featured";
+import Info, { ProductInfo } from "./components/product-info/info";
 
 export type Product = 'no kiero';
 export default function Product(props: Product) {
+  const info: ProductInfo[] = [
+    { title: 'Medidas', subtitle: 'CM', content: '229 x 229 x 90' },
+    { title: 'Capacidad', subtitle: 'Personas', content: '6' },
+    { title: 'Features', subtitle: '', content: 'idk' }
+  ];
   return <main>
     <Hero src="/test.jpg">
       <h1>series crown</h1>
@@ -14,37 +20,21 @@ export default function Product(props: Product) {
       subtitle="Con asientos esculpidos de varios niveles para seis personas, incluida una silla Adirondack de cuerpo completo para una poderosa terapia de cuello a pies. El asiento de remojo súper profundo envuelve en calidez, dirigido a su cuello, hombros y columna vertebral."
       action="Contacta"
     />
-    <ul className="info">
-      <li>
-        <p className="title">Medidas <span>CM</span></p>
-        <p className="content">229 x 229 x 90</p>
-      </li>
-      <li>
-        <p className="title">Capacidad <span>Personas</span></p>
-        <p className="content">6</p>
-      </li>
-      <li>
-        <p className="title">Features <span></span></p>
-        <p className="content">Idk</p>
-      </li>
-      <li>
-        <button className="secondary">More info</button>
-      </li>
-    </ul>
+    <Info info={info} />
     <Parallax src='/test.jpg'></Parallax>
-    <div className="featured">
-      <p className="title">Featured text</p>
-      <p className="subtitle">La nueva colección Crown de Marquis es una obra maestra de innovación. Combina nuestras funciones de terapia más avanzadas y comodidades de lujo con el sistema de hidromasaje más elegante y flexible del mercado.</p>
-    </div>
+    <Featured
+      title="Featured text"
+      subtitle="La nueva colección Crown de Marquis es una obra maestra de innovación. Combina nuestras funciones de terapia más avanzadas y comodidades de lujo con el sistema de hidromasaje más elegante y flexible del mercado."
+    />
     <div className="showcase">
       <img src="/test.jpg" alt="" />
       <img src="/test.jpg" alt="" />
       <button className="secondary">Explora mas caracteristicas</button>
     </div>
-    <div className="featured">
-      <p className="title">Featured text</p>
-      <p className="subtitle">La nueva colección Crown de Marquis es una obra maestra de innovación. Combina nuestras funciones de terapia más avanzadas y comodidades de lujo con el sistema de hidromasaje más elegante y flexible del mercado.</p>
-    </div>
+    <Featured
+      title="Featured text"
+      subtitle="La nueva colección Crown de Marquis es una obra maestra de innovación. Combina nuestras funciones de terapia más avanzadas y comodidades de lujo con el sistema de hidromasaje más elegante y flexible del mercado."
+    />
     <ul className="gallery">
       <li>
         <img src="/test.jpg" />
@@ -60,5 +50,5 @@ export default function Product(props: Product) {
       </li>
       <button>Contacta con un proveedor</button>
     </ul>
-  </main>
+  </main >
 }
