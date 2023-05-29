2474865ba1f03ac9f1947db38eee6d61af4ac057
daiant <cmg2512@gmail.com>
2023-05-29 19:58:18 +0200

    feat(featured): usage of button

--- src/app/product/components/featured/featured.tsx
+++ src/app/product/components/featured/featured.tsx
@@ -1,9 +1,10 @@
+import Button from '@/components/button/button';
 import styles from './featured.module.css';
 
 export default function Featured(props: { title: string, subtitle: string, action?: string, handleAction?: any }) {
   return <div className={styles.featured}>
     <p className={styles.title}>{props.title}</p>
     <p className={styles.subtitle}>{props.subtitle}</p>
-    {props.action && <button className={styles.button} onClick={props.handleAction}>{props.action}</button>}
+    {props.action && <Button handleAction={props.handleAction}>{props.action}</Button>}
   </div>
 }
