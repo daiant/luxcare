b20758f7c06eb3a3e9d123c125e798b880bd9c74
daiant <cmg2512@gmail.com>
2023-05-29 19:38:56 +0200

    feat(product): create page skeleton

---
+++ src/app/product/page.tsx
@@ -0,0 +1,63 @@
+import Hero from "@/components/hero/hero";
+import Parallax from "@/components/parallax/parallax";
+
+export type Product = 'no kiero';
+export default function Product(props: Product) {
+  return <main>
+    <Hero>
+      <h1>series crown</h1>
+      <h2>Marquis deep sea</h2>
+    </Hero>
+    <div className="featured">
+      <p className="title">Una verdadera leyenda</p>
+      <p className="subtitle">Con asientos esculpidos de varios niveles para seis personas, incluida una silla Adirondack de cuerpo completo para una poderosa terapia de cuello a pies. El asiento de remojo súper profundo envuelve en calidez, dirigido a su cuello, hombros y columna vertebral. </p>
+      <button className="action">Contacta</button>
+    </div>
+    <ul className="info">
+      <li>
+        <p className="title">Medidas <span>CM</span></p>
+        <p className="content">229 x 229 x 90</p>
+      </li>
+      <li>
+        <p className="title">Capacidad <span>Personas</span></p>
+        <p className="content">6</p>
+      </li>
+      <li>
+        <p className="title">Features <span></span></p>
+        <p className="content">Idk</p>
+      </li>
+      <li>
+        <button className="secondary">More info</button>
+      </li>
+    </ul>
+    <Parallax src='/test.jpg'></Parallax>
+    <div className="featured">
+      <p className="title">Featured text</p>
+      <p className="subtitle">La nueva colección Crown de Marquis es una obra maestra de innovación. Combina nuestras funciones de terapia más avanzadas y comodidades de lujo con el sistema de hidromasaje más elegante y flexible del mercado.</p>
+    </div>
+    <div className="showcase">
+      <img src="/test.jpg" alt="" />
+      <img src="/test.jpg" alt="" />
+      <button className="secondary">Explora mas caracteristicas</button>
+    </div>
+    <div className="featured">
+      <p className="title">Featured text</p>
+      <p className="subtitle">La nueva colección Crown de Marquis es una obra maestra de innovación. Combina nuestras funciones de terapia más avanzadas y comodidades de lujo con el sistema de hidromasaje más elegante y flexible del mercado.</p>
+    </div>
+    <ul className="gallery">
+      <li>
+        <img src="/test.jpg" />
+        <p>El flujo laminar optimiza el flujo de agua al eliminar la turbulencia entre las bombas y los chorros. Como resultado, experimenta una penetración más profunda del tejido muscular sin causar molestias en la piel. En otras palabras, "¡Alto flujo, sin picadura!".</p>
+      </li>
+      <li>
+        <img src="/test.jpg" />
+        <p>El flujo laminar optimiza el flujo de agua al eliminar la turbulencia entre las bombas y los chorros. Como resultado, experimenta una penetración más profunda del tejido muscular sin causar molestias en la piel. En otras palabras, "¡Alto flujo, sin picadura!".</p>
+      </li>
+      <li>
+        <img src="/test.jpg" />
+        <p>El flujo laminar optimiza el flujo de agua al eliminar la turbulencia entre las bombas y los chorros. Como resultado, experimenta una penetración más profunda del tejido muscular sin causar molestias en la piel. En otras palabras, "¡Alto flujo, sin picadura!".</p>
+      </li>
+      <button>Contacta con un proveedor</button>
+    </ul>
+  </main>
+}
