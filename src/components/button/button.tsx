36e1103fe4b545c017c92c5c7b91a631e1781248
daiant <cmg2512@gmail.com>
2023-05-31 15:43:28 +0200

    fix(button): secondary styles

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { ReactNode } from "react"
 import styles from './button.module.css';
 
@@ -5,7 +7,7 @@ export type ButtonVariant = "primary" | "secondary"
 export default function Button(props: { variant?: ButtonVariant, handleAction: any, children: ReactNode }) {
   function getClassname(): string {
     const classList = [styles.button];
-    if (props.variant) { classList.push(`styles.${props.variant}`); }
+    if (props.variant) { classList.push(styles.secondary); }
     return classList.join(" ");
   }
   return <button onClick={props.handleAction} className={getClassname()}>{props.children}</button>
