17744c2bd3ef7573c7fcd56272318efe7dca03d4
daiant <cmg2512@gmail.com>
2023-06-06 18:27:49 +0200

    feat(footer): new component footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,29 @@
+import Link from 'next/link';
 import styles from './footer.module.css';
 
 export default function Footer() {
-  return <footer className={styles.footer}><h1>SOY UN FOOOTER</h1></footer>
+  return <footer className={styles.footer}>
+    <header className={styles.logo}>
+      <p className={styles.title}>L U X C A R E</p>
+    </header>
+    <section className={styles.links}>
+      <ul className={styles.list}>
+        <Link className={styles.link} href='/about'>About us</Link>
+        <Link className={styles.link} href='/about'>Experiences</Link>
+        <Link className={styles.link} href='/about'>Our Spas</Link>
+        <Link className={styles.link} href='/about'>Complements</Link>
+        <Link className={styles.link} href='/about'>Contact</Link>
+      </ul>
+      <ul className={styles.list}>
+        <Link className={styles.link} href="/crown">Serie Crown</Link>
+        <Link className={styles.link} href="/vector">Serie Vector</Link>
+        <Link className={styles.link} href="/strato">Serie Strato</Link>
+      </ul>
+      <ul className={styles.list}>
+        <Link className={styles.link} href="/about">Privacy</Link>
+        <Link className={styles.link} href="/about">Cookies</Link>
+        <Link className={styles.link} href="/about">Terms of Service</Link>
+      </ul>
+    </section>
+  </footer>
 }
