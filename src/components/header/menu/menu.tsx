78232d968f2c3e866da6f3334276a25339e660f1
charlo <cmg2512@gmail.com>
2024-03-14 11:35:07 +0100

    fix: menu

--- src/components/header/menu/menu.tsx
+++ src/components/header/menu/menu.tsx
@@ -2,9 +2,8 @@
 
 import { useEffect, useState } from 'react';
 import styles from './menu.module.css';
-import { Routes, routes } from './menu.routing';
+import { Routes } from './menu.routing';
 import Link from 'next/link';
-import Image from 'next/image';
 export default function Menu(props: { visible: boolean, onClick: any }) {
   const [children, setChildren] = useState<Routes>([]);
   const [opacity, setOpacity] = useState(1);
@@ -31,32 +30,30 @@ export default function Menu(props: { visible: boolean, onClick: any }) {
     <div className={styles.close} onClick={props.onClick} aria-hidden={!props.visible}>
       <img src="/close.svg" className={styles['close--img']} alt="" />
     </div>
-    <menu aria-hidden={!props.visible} className={styles.menu}>
-      <ul className={styles.list} aria-hidden={Boolean(children.length)}>
-        {routes.map((route, index) => (
-          <li key={index} className={styles.item}>
-            {!route.routes && <Link href={route.url} className={styles['item--link']} onClick={props.onClick}>{route.name}</Link>}
-            {route.routes && <span className={styles['item--link']} onClick={() => handleChildren(route.routes)}>
-              {route.name}
-              {/* <img src="/next.svg" alt="" className={styles.img} /> */}
-            </span>}
-          </li>
-        ))}
-      </ul>
-    </menu>
-    {children.length > 0 && <div className={styles.submenu} style={{ '--opacity': opacity } as React.CSSProperties}>
-      <ul className={styles.list}>
-        <p onClick={restoreChildren} className={styles.back}>Back</p>
-        {children.map((route, index) => <li key={route.name + index} className={styles['submenu--list--item']}>
-          <Link href={route.url} onClick={props.onClick} className={styles['submenu--list--item--link']}>
-            <Image src="/test.webp" alt='' className={styles.image} fill></Image>
-          </Link>
-          <div className={styles.info}>
-            <h2>{route.name}</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda libero quos iste sunt harum aspernatur sit neque nam asperiores quisquam ut perspiciatis, exercitationem atque illum. Error esse quae suscipit voluptatum.</p>
-          </div>
-        </li>)}
-      </ul>
-    </div>}
-  </div>
+    <article className={styles.menu} aria-hidden={!props.visible}>
+      <div role='contentgroup'>
+        <ul>
+          <li><Link href='/'>Inicio</Link></li>
+        </ul>
+      </div>
+      <div role='contentgroup'>
+        <ul>
+          <li role='heading'><Link href='/spas'>Series</Link></li>
+          <li><Link href='/spas/vector'>Vector</Link></li>
+          <li><Link href='/spas/strato'>Strato</Link></li>
+          <li><Link href='/spas/crown'>Crown</Link></li>
+        </ul>
+      </div>
+      <div role='contentgroup'>
+        <ul>
+          <li><Link href='/recommendations'>Recomendaciones</Link></li>
+        </ul>
+      </div>
+      <div role='contentgroup'>
+        <ul>
+          <li><Link href='/contact'>Contacto</Link></li>
+        </ul>
+      </div>
+    </article>
+  </div >
 }
