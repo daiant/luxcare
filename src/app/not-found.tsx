a49c75394d4424aa03d48a64ea39acf9e0327f11
charlo <cmg2512@gmail.com>
2024-05-10 18:38:22 +0200

    feat: add not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,21 @@
-import Link from 'next/link'
+import styles from '@/styles/not-found.module.css';
+import Border from '@/public/404/border.png';
+import Logo from '@/public/404/404.png';
+import Link from 'next/link';
+import Caret from '@/public/icons/CaretRight.svg';
 
 export default function NotFound() {
   return (
-    <div style={{ minHeight: '100vh', display: 'grid', placeContent: 'center' } as React.CSSProperties}>
-      <h2 style={{ fontSize: 40 }}>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/" style={{ color: 'var(--color)' }}>Return Home</Link>
+    <div className={styles.container}>
+      <div>
+        <img src={Border.src} alt="" className={styles.border} />
+        <img src={Logo.src} alt="" className={styles.logo + ' not-found-rotate'} />
+      </div>
+      <Link href={'/'}>
+        <span>Volver al inicio
+        </span>
+        <Caret className={styles.caret} />
+      </Link>
     </div>
   )
 }
