0f4cf85041c902a401572c22b7fb3f53620389fc
charlo <cmg2512@gmail.com>
2024-05-11 13:29:36 +0200

    feat: footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,31 +5,30 @@ import Image from 'next/image';
 export default function Footer() {
   return <footer className={styles.footer}>
     <div className={styles.header}>
-      <div className={styles.main}>
-        <Image src={'/LuxCare_c.svg'} width={0} height={0} sizes="100vw" alt='luxcare' />
-      </div>
       <div className={styles.cta}>
         <p>Cada día más<br />cerca de nuestros clientes</p>
-        <Link href={'/contact'}>Contacta</Link>
       </div>
-      <div className={styles.links}>
-        <div className={styles.group}>
-          <Link href='/faq'>Preguntas frecuentes</Link>
-          <Link href='/careers'>Trabaja con nosotros</Link>
-          <Link href='/appointment'>¿Te llamamos?</Link>
-        </div>
-        <div className={styles.group}>
-          <Link href='www.instagram.com' target='_blank'>Instagram</Link>
-          <Link href='www.twitter.com' target='_blank'>X</Link>
-          <Link href='www.facebook.com' target='_blank'>Facebook</Link>
-          <Link href='www.linkedin.com' target='_blank'>LinkedIn</Link>
-        </div>
+      <div className={styles.main}>
+        <p>LuxCare Spas</p>
+        <span>Copyright 2024 LuxCare Spas</span>
       </div>
     </div>
-    <div className={styles.legal}>
-      <Link href='/legal/privacy'>Privacidad</Link>
-      <Link href='/legal/cookies'>Cookies</Link>
-      <Link href='/legal/tos'>Condiciones</Link>
+    <div className={styles.links}>
+      <div className={styles.group}>
+        <Link href='/faq'>Preguntas frecuentes</Link>
+        <Link href='/careers'>Trabaja con nosotros</Link>
+        <Link href='/appointment'>¿Te llamamos?</Link>
+      </div> <div className={styles.group}>
+        <Link href='www.instagram.com' target='_blank'>Instagram</Link>
+        <Link href='www.twitter.com' target='_blank'>X</Link>
+        <Link href='www.facebook.com' target='_blank'>Facebook</Link>
+        <Link href='www.linkedin.com' target='_blank'>LinkedIn</Link>
+      </div>
+      <div className={styles.group}>
+        <Link href='/legal/privacy'>Privacidad</Link>
+        <Link href='/legal/cookies'>Cookies</Link>
+        <Link href='/legal/tos'>Condiciones</Link>
+      </div>
     </div>
   </footer>
 }
