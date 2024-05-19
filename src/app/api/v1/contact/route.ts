3d5f1f08186efc4b88d87acdcd5ff249e061569a
daiant <cmg2512@gmail.com>
2024-05-19 12:44:48 +0200

    fix: runtime

--- src/app/api/v1/contact/route.ts
+++ src/app/api/v1/contact/route.ts
@@ -2,15 +2,13 @@ import { sql } from '@vercel/postgres';
 
 export async function POST(req: Request) {
   const body = await req.json()
-  if (!body.token) return Response.json({ error: "No token found" }, {
-    status: 400
-  });
+  if (!body.token) return new Response(JSON.stringify({ error: "no token" }), { status: 400 });
   const tokenResponse = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA}&response=${body.token}`, { method: "POST" })
   const { score } = await tokenResponse.json()
-  if (score < 0.5) return Response.json({ error: "Are you a bot?" }, { status: 401 });
+  if (score < 0.5) return new Response(JSON.stringify({ error: "Are you a bot?" }), { status: 401 });
 
   console.log("oleeeee todo bien todo bien");
   await sql`INSERT INTO customer_contacts (name, email, phone, comments) VALUES (${body.name}, ${body.email}, ${body.phone}, ${body.question});`;
 
-  return Response.json({ 'msg': 'holi' })
+  return new Response(JSON.stringify({ msg: 'todo bien' }));
 }
