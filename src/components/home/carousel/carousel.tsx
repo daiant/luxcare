e5c99f1a611266cfbbf5f73a169951bb35041c2e
csendra <csendra@power-electronics.com>
2024-06-08 12:19:57 +0200

    feat: images

--- src/components/home/carousel/carousel.tsx
+++ src/components/home/carousel/carousel.tsx
@@ -1,13 +1,8 @@
 'use client';
-import photo1 from '@/public/images/home/destino/destino_01.webp';
-import photo2 from '@/public/images/home/destino/destino_02.webp';
-import photo3 from '@/public/images/home/destino/destino_03.webp';
-import photo4 from '@/public/images/home/destino/destino_04.webp';
-import photo6 from '@/public/images/home/destino/destino_06.webp';
-import photo7 from '@/public/images/home/destino/destino_07.webp';
 import styles from './carousel.module.css';
 import LongLine from '@/public/images/icons/long_line.svg';
 import React from 'react';
+import { fetchImage } from '@/lib/fetch-image';
 
 let pos = { x: 0, y: 0, left: 0, top: 0 }
 export default function HomeCarouselComponent() {
@@ -63,37 +58,37 @@ export default function HomeCarouselComponent() {
     <div className={styles.carousel} onMouseDown={handleMouseDown} ref={slider}>
       <div className={styles.item}>
         <div className={styles.img_wrapper}>
-          <img draggable="false" alt="crown" src={photo1.src} />
+          <img draggable="false" alt="crown" src={fetchImage('/images/home/destino/destino_01.webp')} />
         </div>
         <p><strong>Innovación, bienestar, experiencia</strong>: te ofrecemos un oasis de relajación y rejuvenecimiento con una variedad de servicios diseñados para revitalizar tanto tu cuerpo como tu mente.</p>
       </div>
       <div className={styles.item}>
         <div className={styles.img_wrapper}>
-          <img draggable="false" alt="vector" src={photo2.src} />
+          <img draggable="false" alt="vector" src={fetchImage('/images/home/destino/destino_02.webp')} />
         </div>
         <p><strong>Nuestro objetivo es simple</strong>: continuar innovando para estar cada día más cerca de nuestros clientes. Queremos ofrecer soluciones y productos que hagan que su vida sea más relajada y confortable.</p>
       </div>
       <div className={styles.item}>
         <div className={styles.img_wrapper}>
-          <img draggable="false" alt="strato" src={photo3.src} />
+          <img draggable="false" alt="strato" src={fetchImage('/images/home/destino/destino_03.webp')} />
         </div>
         <p><strong>En LuxCare</strong>, cada compra se convierte en una experiencia sensorial auténtica. Nos comprometemos con la perfección y nos respaldamos por sólidas garantías</p>
       </div>
       <div className={styles.item}>
         <div className={styles.img_wrapper}>
-          <img draggable="false" alt="vector" src={photo4.src} />
+          <img draggable="false" alt="vector" src={fetchImage('/images/home/destino/destino_07.webp')} />
         </div>
         <p>Buscamos <strong>conectar con las emociones</strong> mientras garantizamos la más alta calidad. Somos sinónimo de compromiso y autenticidad: nos esforzamos por ofrecer algo más que un producto, creamos una experiencia única donde cada detalle cuenta.</p>
       </div>
       <div className={styles.item}>
         <div className={styles.img_wrapper}>
-          <img draggable="false" alt="crown" src={photo7.src} />
+          <img draggable="false" alt="crown" src={fetchImage('/images/home/destino/destino_05.webp')} />
         </div>
         <p> Ofrecemos una selección exclusiva de spas de primera calidad, cuidadosamente seleccionados por su calidad, diseño y características innovadoras. Cada cliente es recibido con una atención individualizada, donde se les escucha atentamente para comprender sus necesidades específicas.</p>
       </div>
       <div className={styles.item}>
         <div className={styles.img_wrapper}>
-          <img draggable="false" alt="crown" src={photo6.src} />
+          <img draggable="false" alt="crown" src={fetchImage('/images/home/destino/destino_06.webp')} />
         </div>
         <p> Es por eso que nos esforzamos por crear un ambiente donde cada detalle destaque por su <strong>autenticidad y perfección</strong>, respaldado por sólidas garantías que refuerzan nuestro compromiso de calidad.</p>
       </div>
