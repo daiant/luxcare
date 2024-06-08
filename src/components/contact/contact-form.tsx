c1e8d871dce51a4ff9208ffa8166a98d5f280103
csendra <csendra@power-electronics.com>
2024-06-08 12:32:54 +0200

    feat(contact): tos

--- src/components/contact/contact-form.tsx
+++ src/components/contact/contact-form.tsx
@@ -5,18 +5,8 @@ import Input from '../form/input/input';
 import Textarea from '../form/textarea/textarea';
 import React from 'react';
 export default function ContactForm({ onSubmit, submitted, className }: { onSubmit: FormEventHandler<HTMLFormElement>, submitted: boolean, className?: string }) {
-  const [missingTOS, setMissingTOS] = React.useState(false);
 
-  function checkTOS($event: React.FormEvent<HTMLFormElement>) {
-    $event.preventDefault()
-    const data = new FormData($event.target as HTMLFormElement);
-    if (data.get('tos')) onSubmit($event);
-    else {
-      setMissingTOS(true);
-      setTimeout(() => { setMissingTOS(false); }, 2000)
-    }
-  }
-  return <form className={`${styles.form} ${className}`} autoComplete='off' onSubmit={checkTOS}>
+  return <form className={`${styles.form} ${className}`} autoComplete='off' onSubmit={onSubmit}>
     <Input required={true} label='Nombre' name='name' type='text' ></Input>
     <Input required={true} label='Email' name='email' type='email' ></Input>
     <Input required={true} label='Teléfono' name='phone' type='tel' ></Input>
@@ -24,9 +14,8 @@ export default function ContactForm({ onSubmit, submitted, className }: { onSubm
 
     <div className={styles.actions}>
       <span aria-hidden={!submitted} className={styles.info}>Mensaje enviado correctamente!</span>
-      <label className={missingTOS ? styles.missingTOS : ''}>
-        <input type="checkbox" name="tos" id="tos" />
-        <span>Acepto los <a href="/legal/tos" target="_blank">términos y condiciones</a></span>
+      <label className={styles.tos}>
+        Al hacer click estoy aceptando los <a href='/legal/tos'>Términos y condiciones</a>.
       </label>
       <button className={styles.button}>Contactar</button>
     </div>
